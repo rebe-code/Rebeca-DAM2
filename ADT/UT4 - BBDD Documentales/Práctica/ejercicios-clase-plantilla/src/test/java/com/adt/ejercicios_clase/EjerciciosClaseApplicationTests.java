53d170789460644d154eda82c435b82df9ea3e64
Rebeca Pavon Fernandez <rebecapf63@educastur.es>
2026-01-20 17:34:56 +0100

    varias

---
+++ ADT/UT4 - BBDD Documentales/Práctica/ejercicios-clase-plantilla/src/test/java/com/adt/ejercicios_clase/EjerciciosClaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adt.ejercicios_clase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjerciciosClaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
