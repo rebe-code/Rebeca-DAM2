e7753f0832ffc197ef9a42c866239daf3ff045f8
rebe-code <145746067+rebe-code@users.noreply.github.com>
2026-02-03 13:23:18 +0100

    teoria subrayada

---
+++ Android Studio clase/RecyclerView2-Clase/RecyclerView2-Clase/app/src/main/java/com/example/recyclerview/Receta.kt
@@ -0,0 +1,12 @@
+package com.example.recyclerview
+
+//class Persona (var color: String = "", var nombre: String = "", var ciudad: String = "", var estado: String = "")
+
+
+class Receta(color: String, nombre: String, tiempo: String, dificultad: String, foto:Int){
+    var color:String=color
+    var nombre:String=nombre
+    var tiempo:String=tiempo
+    var dificultad:String= dificultad
+    var foto:Int = foto
+}
