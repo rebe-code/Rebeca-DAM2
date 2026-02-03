0e26bb64f25d9f2adeafd55a2638403db71f2777
Rebeca Pavon Fernandez <rebecapf63@educastur.es>
2026-02-03 19:35:23 +0100

    proyectos android studio

---
+++ ADT/UT4 - BBDD Documentales/Práctica/ejercicios-clase-plantilla/src/test/java/com/adt/ejercicios_clase/service/MensajeServiceTest.java
@@ -0,0 +1,19 @@
+package com.adt.ejercicios_clase.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MensajeServiceTest {
+    @Autowired
+    private MensajeService mensajeService;
+
+    @Test
+    void mensajesDeUser() {
+    }
+
+    @Test
+    void mensajesDeUserYRoom() {
+    }
+}
